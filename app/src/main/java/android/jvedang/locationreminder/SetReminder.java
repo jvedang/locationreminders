a768d4a1f7880d775974e89d6eb613c89e197b29
Vedang <jvedang@gmail.com>
2016-07-09 12:41:43 -0700

    Added Google Places Autocomplete Text

--- app/src/main/java/android/jvedang/locationreminder/SetReminder.java
+++ app/src/main/java/android/jvedang/locationreminder/SetReminder.java
@@ -2,27 +2,53 @@ package android.jvedang.locationreminder;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
+import android.net.Uri;
 import android.os.Build;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.Html;
+import android.text.Spanned;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.AutoCompleteTextView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.places.AutocompletePrediction;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.PlaceBuffer;
+import com.google.android.gms.location.places.Places;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 public class SetReminder extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener,OnMapReadyCallback,GoogleMap.OnMapClickListener{
@@ -33,7 +59,12 @@ public class SetReminder extends AppCompatActivity implements GoogleApiClient.Co
     protected GoogleApiClient googleApiClient;
     private GoogleMap mMap;
     private SupportMapFragment mapFragment;
-    private double latitude, longitude;
+    private double latitude, longitude, resultLatitude, resultLongitude;
+    private PlaceAutocompleteAdapter mAdapter;
+    private AutoCompleteTextView mAutocompleteView;
+    private TextView mPlaceDetailsText;
+    private TextView mPlaceDetailsAttribution;
+    private Marker marker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -85,7 +116,15 @@ public class SetReminder extends AppCompatActivity implements GoogleApiClient.Co
 
         buildGoogleApiClient();
 
+        // Retrieve the AutoCompleteTextView that will display Place suggestions.
+        mAutocompleteView = (AutoCompleteTextView)
+                findViewById(R.id.autocomplete_places);
 
+        // Register a listener that receives callbacks when a suggestion has been selected
+        mAutocompleteView.setOnItemClickListener(mAutocompleteClickListener);
+
+        // Retrieve the TextViews that will display details and attributions of the selected place.
+        mPlaceDetailsText = (TextView) findViewById(R.id.place_details);
 
     }
 
@@ -97,6 +136,8 @@ public class SetReminder extends AppCompatActivity implements GoogleApiClient.Co
                 .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this)
                 .addApi(LocationServices.API)
+                .enableAutoManage(this, 0 /* clientId */, this)
+                .addApi(Places.GEO_DATA_API)
                 .build();
     }
 
@@ -157,6 +198,13 @@ public class SetReminder extends AppCompatActivity implements GoogleApiClient.Co
 
         }
 
+        LatLngBounds CURRENT_LOCATION = new LatLngBounds(
+                new LatLng(latitude, longitude), new LatLng(latitude, longitude));
+        // Set up the adapter that will retrieve suggestions from the Places Geo Data API that cover
+        // the entire world.
+        mAdapter = new PlaceAutocompleteAdapter(this, googleApiClient, CURRENT_LOCATION,
+                null);
+        mAutocompleteView.setAdapter(mAdapter);
         mapFragment.getMapAsync(SetReminder.this);
         Log.i(TAG, "Connected to GoogleApiClient");
         Log.i(TAG,"Latitude - "+latitude+" and Longitude - "+longitude);
@@ -171,11 +219,6 @@ public class SetReminder extends AppCompatActivity implements GoogleApiClient.Co
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
         Log.i(TAG, "Connection failed: ConnectionResult.getErrorCode() = " + connectionResult.getErrorCode());
-    }
-
-    @Override
-    public void onMapClick(LatLng latLng) {
-
     }
 
     @Override
@@ -253,6 +296,141 @@ public class SetReminder extends AppCompatActivity implements GoogleApiClient.Co
                 break;
             // other 'case' lines to check for other
             // permissions this app might request
+        }
+    }
+
+    /**
+     * Listener that handles selections from suggestions from the AutoCompleteTextView that
+     * displays Place suggestions.
+     * Gets the place id of the selected item and issues a request to the Places Geo Data API
+     * to retrieve more details about the place.
+     *
+     * @see com.google.android.gms.location.places.GeoDataApi#getPlaceById(com.google.android.gms.common.api.GoogleApiClient,
+     * String...)
+     */
+    private AdapterView.OnItemClickListener mAutocompleteClickListener
+            = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            /*
+             Retrieve the place ID of the selected item from the Adapter.
+             The adapter stores each Place suggestion in a AutocompletePrediction from which we
+             read the place ID and title.
+              */
+            final AutocompletePrediction item = mAdapter.getItem(position);
+            final String placeId = item.getPlaceId();
+            final CharSequence primaryText = item.getPrimaryText(null);
+
+            Log.i(TAG, "Autocomplete item selected: " + primaryText);
+
+            /*
+             Issue a request to the Places Geo Data API to retrieve a Place object with additional
+             details about the place.
+              */
+            PendingResult<PlaceBuffer> placeResult = Places.GeoDataApi
+                    .getPlaceById(googleApiClient, placeId);
+            placeResult.setResultCallback(mUpdatePlaceDetailsCallback);
+
+            Toast.makeText(getApplicationContext(), "Clicked: " + primaryText,
+                    Toast.LENGTH_SHORT).show();
+            Log.i(TAG, "Called getPlaceById to get Place details for " + placeId);
+        }
+    };
+
+    /**
+     * Callback for results from a Places Geo Data API query that shows the first place result in
+     * the details view on screen.
+     */
+    private ResultCallback<PlaceBuffer> mUpdatePlaceDetailsCallback
+            = new ResultCallback<PlaceBuffer>() {
+        @Override
+        public void onResult(PlaceBuffer places) {
+            if (!places.getStatus().isSuccess()) {
+                // Request did not complete successfully
+                Log.e(TAG, "Place query did not complete. Error: " + places.getStatus().toString());
+                places.release();
+                return;
+            }
+            // Get the Place object from the buffer.
+            final Place place = places.get(0);
+
+            resultLatitude = place.getLatLng().latitude;
+            resultLongitude = place.getLatLng().longitude;
+            // Format details of the place for display and show it in a TextView.
+            mPlaceDetailsText.setText(place.getAddress().toString());
+
+            Log.i(TAG, "Place details received: " + place.getName());
+
+            if(marker != null){
+                marker.remove();
+
+            }
+
+            LatLng latLng = new LatLng(resultLatitude,resultLongitude);
+
+            marker = mMap.addMarker(new MarkerOptions().position(latLng).title(place.getAddress().toString()));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+            // Zoom in, animating the camera.
+            mMap.animateCamera(CameraUpdateFactory.zoomIn());
+
+            // Zoom out to zoom level 10, animating with a duration of 2 seconds.
+            mMap.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+
+            // Construct a CameraPosition focusing on Mountain View and animate the camera to that position.
+            CameraPosition cameraPosition = new CameraPosition.Builder()
+                    .target(latLng)      // Sets the center of the map to Mountain View
+                    .zoom(15)                   // Sets the zoom
+                    .bearing(90)                // Sets the orientation of the camera to east
+                    // Sets the tilt of the camera to 30 degrees
+                    .build();                   // Creates a CameraPosition from the builder
+            mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
+
+            places.release();
+        }
+    };
+
+    private static Spanned formatPlaceDetails(Resources res, CharSequence name, String id,
+                                              CharSequence address, CharSequence phoneNumber, Uri websiteUri) {
+        Log.e(TAG, res.getString(R.string.place_details, name, id, address, phoneNumber,
+                websiteUri));
+        return Html.fromHtml(res.getString(R.string.place_details, name, id, address, phoneNumber,
+                websiteUri));
+
+    }
+
+    @Override
+    public void onMapClick(LatLng latLng) {
+        if(marker != null){
+            marker.remove();
+
+        }
+
+        Geocoder geocoder = new Geocoder(SetReminder.this, Locale.getDefault());
+        latitude = latLng.latitude;
+        longitude = latLng.longitude;
+        try{
+            List<Address> addresses = geocoder.getFromLocation(latLng.latitude,latLng.longitude,1);
+
+            if(addresses!=null && addresses.size()>0){
+                Address address = addresses.get(0);
+                //countryCode = address.getCountryCode();
+                ArrayList<String> addressFragments = new ArrayList<>();
+
+                for(int i=0; i<address.getMaxAddressLineIndex(); i++){
+                    addressFragments.add(address.getAddressLine(i));
+
+                }
+
+                Toast.makeText(SetReminder.this, TextUtils.join(System.getProperty("line.separator"), addressFragments), Toast.LENGTH_LONG).show();
+                String addressString = TextUtils.join(System.getProperty("line.separator"),addressFragments);
+
+                marker = mMap.addMarker(new MarkerOptions().position(latLng).title(addressString));
+                //textAddress.setText(addressString);
+            }
+        }
+        catch(IOException e){
+            Log.e(TAG,e.getMessage());
         }
     }
 
